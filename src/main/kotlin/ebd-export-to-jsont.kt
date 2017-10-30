0a47b0d2c6ade8686715096dbaed587b8daff305
Mark nenadov <marknenadov@gmail.com>
2017-10-30 06:57:55 -0400

    beginning to experiment with converting edb to json

---
+++ src/main/kotlin/ebd-export-to-jsont.kt
@@ -0,0 +1,32 @@
+import java.io.File
+import java.io.PrintWriter
+
+fun main(args: Array<String> ) {
+    val edbFilePath = "C:\\java\\k-bird\\aebd_CA-ON-ES_relAug-2017\\ebd_CA-ON-ES_relAug-2017.txt"
+    val outputJsonFile = "C:\\java\\k-bird\\aebd_CA-ON-ES_relAug-2017\\output.json"
+    exportToJson( edbFilePath, outputJsonFile )
+}
+
+fun exportToJson(edbFilePath: String, outputJsonFile: String) {
+    val writer = PrintWriter( outputJsonFile )
+
+    val bufferedReader = File( edbFilePath ).bufferedReader()
+    val fieldNames = bufferedReader.readLine().split("\t")
+
+    for (line in bufferedReader.readLines()) {
+        val lineValues = line.split("\t")
+        writer.append("{\n\t")
+        for ((index,value) in lineValues.withIndex() ) {
+            if ( value != "" ) {
+                writer.append("\"${fieldNames[index]}\": \"$value\"")
+                if (index < lineValues.size - 2) {
+                    writer.append(",")
+                }
+                writer.append("\n")
+            }
+        }
+        writer.append("\n}")
+    }
+    writer.close()
+    bufferedReader.close()
+    }
